b7f364a5f47bc7e4f0bb913f133ac053917e5393
Martin Burtscher <martin.burtscher@student.tugraz.at>
2015-03-11 15:45:25 +0100

    updated bluetooth test server

--- CatroidBluetoothTest/startbtserver.sh
+++ CatroidBluetoothTest/startbtserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 echo $BASEDIR
-java -cp $BASEDIR/bluecove-2.1.1-SNAPSHOT.jar:$BASEDIR/bluecove-gpl-2.1.1.jar:$BASEDIR/BTTestNOGUI.jar at.tugraz.ist.catroid.LegoNXTBTTestServer.BTServer $BASEDIR/log.txt 
+java -cp $BASEDIR/bluecove-2.1.1-SNAPSHOT.jar:$BASEDIR/bluecove-gpl-2.1.1.jar:$BASEDIR/BluetoothTestServer_v2.jar org.catrobat.catroid.bluetoothtestserver.BTServer $BASEDIR/log.txt 
